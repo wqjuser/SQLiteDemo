d91096f3e5c8cf41e18c0bd928a5e3786e4050e2
ee <316400644@qq.com>
2017-12-07 10:41:22 +0800

    Initial commit

---
+++ app/src/main/java/com/wqj/mrwen/sqlitedemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.wqj.mrwen.sqlitedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_createDatabase)
+    Button btnCreateDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
---
+++ app/src/main/java/com/wqj/mrwen/sqlitedemo/MyDBHelper.java
@@ -0,0 +1,37 @@
+package com.wqj.mrwen.sqlitedemo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by MR.WEN on 2017/12/7.
+ */
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private Context context;
+    private static final String CREATE_BOOK = "create table Book("
+            + "ID integer primary key autoincrement"
+            + "author text"
+            + "price real"
+            + "pages integer"
+            + "name text)";
+
+
+    public MyDBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_BOOK);
+        Toast.makeText(context, "创建数据库成功", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
